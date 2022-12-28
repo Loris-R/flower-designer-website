fdb6b3174816da5a72d91628a3b3b5a1ed92036e
erika fonseca <fonsecarika@gmail.com>
2022-12-28 17:30:24 +0200

    ok

--- app/javascript/controllers/performances_controller.js
+++ app/javascript/controllers/performances_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["flipedcardone", "sauvagethree", "sauvagefour", "flipedcardtwo", "losmuertosone", "losmuertostwo"]
+  static targets = ["flipedcardone", "sauvagethree", "sauvagefour", "frontcardone", "flipedcardtwo", "losmuertosone", "losmuertostwo"]
 
   translate() {
     const flipedcardone = this.flipedcardoneTarget;
+    const frontcardone = this.frontcardoneTarget;
     const sauvagethree = this.sauvagethreeTarget;
     const sauvagefour = this.sauvagefourTarget;
 
+    frontcardone.classList.toggle("flip");
+    flipedcardone.classList.toggle("visibility");
     flipedcardone.addEventListener('transitionend', () => {
       sauvagethree.classList.toggle("sauvage-three");
       sauvagefour.classList.toggle("sauvage-four");
