6014a2c64112bdb3c80bcfa77aaaa02c5c9ea374
Loris Reynaud <loris.reynaud@gmail.com>
2022-12-31 16:19:36 +0200

    validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,6 @@ class Contact < ApplicationRecord
   has_many :quotings
 
   validates :first_name, :last_name, :email, presence: true
-  validates :email, format: { with: /^[a-zA-Z0-9_-]+\.?[a-zA-Z0-9]+@{1}[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)?\.[a-z]{2,}+$/ }
-  validates :phone_number, format: { with: /^(0|\+33\s?|0033)1-79$/ }
+  validates :email, format: { with: /\A[a-zA-Z0-9_-]+\.?[a-zA-Z0-9]+@{1}[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)?\.[a-z]{2,}+\z/ }
+  validates :phone_number, format: { with: /\A(0|\+33\s?|0033)1-79\z/ }
 end
