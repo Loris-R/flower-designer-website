faebf053e089e922a554bb8177d33f70e12eba56
Loris Reynaud <loris.reynaud@gmail.com>
2023-01-12 14:08:28 +0200

    need to permit categories with an array

--- app/controllers/quotings_controller.rb
+++ app/controllers/quotings_controller.rb
@@ -6,6 +6,17 @@ class QuotingsController < ApplicationController
 
   def create
     @quoting = Quoting.new(quoting_params)
+    contact = Contact.find_or_create_by(email: params[:quoting][:contact_attributes][:email]) do |contact|
+      contact.first_name = params[:quoting][:contact_attributes][:first_name]
+      contact.last_name = params[:quoting][:contact_attributes][:last_name]
+      contact.city = params[:quoting][:contact_attributes][:city] if params[:quoting][:contact_attributes][:city]
+      contact.phone_number = params[:quoting][:contact_attributes][:phone_number] if params[:quoting][:contact_attributes][:phone_number]
+    end
+
+      categories = params[:quoting][:categories].delete_at(0)
+      @quoting.contact = contact
+      @quoting.categories = categories
+      raise
 
     if @quoting.save!
       redirect_to root_path
@@ -17,6 +28,7 @@ class QuotingsController < ApplicationController
   private
 
   def quoting_params
-    params.require(:quoting).permit(:contact_id, :address, :zipcode, :city, :event_date)
+    params.require(:quoting).permit(:address, :zipcode, :city, :event_date, 
+      contact_attributes: [:first_name, :last_name, :email, :phone_number, :city])
   end
 end
--- app/models/quoting.rb
+++ app/models/quoting.rb
@@ -2,7 +2,7 @@ class Quoting < ApplicationRecord
   belongs_to :contact
   accepts_nested_attributes_for :contact
 
-  validates :address, :zipcode, :city, :event_date, :category, presence: true
+  validates :address, :zipcode, :city, :event_date, :categories, presence: true
 
   CATEGORIES = %w(Mariage Cortège Mairie Lieux\ religieux Cérémonie\ Laïque Salle\ de\ réception Livraison/Démontage Baby\ shower Demande\ en\ mariage Anniversaire Fête\ religieuse Séminaire Repas\ professionnel Autre)
 end
