fc742015b6b4849672852d7dd93d27d93126f2e4
Loris Reynaud <loris.reynaud@gmail.com>
2023-01-20 20:25:24 +0100

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,14 @@ Rails.application.configure do
   # mailer configuration
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "https://alicia-staging.herokuapp.com/" }
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_SERVER'],
+    port: ENV['SMTP_PORT'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
