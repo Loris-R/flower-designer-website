a7657482078bc5cc4cf14117f54eaf2520524a84
Loris Reynaud <loris.reynaud@gmail.com>
2023-01-20 17:19:47 +0100

    corrected

--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -4,4 +4,9 @@ class MessageMailerPreview < ActionMailer::Preview
   def incoming
     MessageMailer.with(message: Message.last, contact: Message.last.contact).incoming
   end
+
+  # Preview this email at http://localhost:3000/rails/mailers/message_mailer/thanks
+  def thanks
+    MessageMailer.with(message: Message.last, contact: Message.last.contact).thanks
+  end
 end
--- test/mailers/previews/quoting_mailer_preview.rb
+++ test/mailers/previews/quoting_mailer_preview.rb
@@ -3,7 +3,11 @@ class QuotingMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/quoting_mailer/incoming
   def incoming
-    QuotingMailer.incoming
+    QuotingMailer.with(quoting: Quoting.last, contact: Quoting.last.contact).incoming
   end
-
+    
+  # Preview this email at http://localhost:3000/rails/mailers/quoting_mailer/thanks
+  def thanks
+    QuotingMailer.with(quoting: Quoting.last, contact: Quoting.last.contact).thanks
+  end
 end
